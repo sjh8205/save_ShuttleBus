4d8504b86d80778d99b6859707b2b6147da430dc
sonhyejin <sjh8205@naver.com>
2018-09-30 16:48:14 +0900

    write splash code

--- app/src/main/java/com/example/sonhyejin/save_shuttlebus/SplashActivity.java
+++ app/src/main/java/com/example/sonhyejin/save_shuttlebus/SplashActivity.java
@@ -1,4 +1,21 @@
 package com.example.sonhyejin.save_shuttlebus;
 
-public class SplashActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SplashActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        try{
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+
+        startActivity(new Intent(this, P_main.class));
+        finish();
+    }
 }
