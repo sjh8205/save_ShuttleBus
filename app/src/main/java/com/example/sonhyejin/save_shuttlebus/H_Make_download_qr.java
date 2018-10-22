820f5759a0bb27901217413a5a76c546c504691f
jeewoo9719 <34128041+jeewoo9719@users.noreply.github.com>
2018-10-22 13:09:02 +0900

    10.18 추가
    firebase 추가
    child register 부분
    register_child랑 make_download_qr 액티비티 생성

--- app/src/main/java/com/example/sonhyejin/save_shuttlebus/H_Make_download_qr.java
+++ app/src/main/java/com/example/sonhyejin/save_shuttlebus/H_Make_download_qr.java
@@ -3,11 +3,11 @@ package com.example.sonhyejin.save_shuttlebus;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class T_QRScan extends AppCompatActivity {
-//선생님은 원생들 qr 스캔하는 페이지?
+public class H_Make_download_qr extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_t_qrscan);
+        setContentView(R.layout.activity_h__make_download_qr);
     }
 }
