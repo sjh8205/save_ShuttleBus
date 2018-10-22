ab7403e990f1fc6b59f65f26f8799218096641fa
jeewoo9719 <34128041+jeewoo9719@users.noreply.github.com>
2018-10-22 13:55:09 +0900

    원생 정보등록
    원생 정보 등록하기
    이름, 부모번호, 유치원반, 버스타는지, 어디서 타는지(우선은 txt로)

--- app/src/main/java/com/example/sonhyejin/save_shuttlebus/H_Register_child.java
+++ app/src/main/java/com/example/sonhyejin/save_shuttlebus/H_Register_child.java
@@ -1,13 +1,44 @@
 package com.example.sonhyejin.save_shuttlebus;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class H_Register_child extends AppCompatActivity {
-
+    boolean busRideStatus;
+    EditText station;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_h__register_child);
+
+
+
+        EditText childName = (EditText) findViewById(R.id.childName);
+        EditText childClass = (EditText) findViewById(R.id.childClass);
+        EditText phoneNum = (EditText) findViewById(R.id.parentPhoneNum);
+
+        final RadioGroup busRide=(RadioGroup)findViewById(R.id.busStatusGroup);
+        final LinearLayout layout=(LinearLayout)findViewById(R.id.busStation);
+
+        RadioGroup.OnCheckedChangeListener radiogroupChange=new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group,@IdRes int checkedId) {
+                if(checkedId==R.id.busYes){
+                    busRideStatus=true;
+                    layout.setVisibility(View.VISIBLE);
+                    station = (EditText) findViewById(R.id.childBusStation);
+                }
+                else{
+                    busRideStatus=false;
+                }
+            }
+        };
+
     }
 }
