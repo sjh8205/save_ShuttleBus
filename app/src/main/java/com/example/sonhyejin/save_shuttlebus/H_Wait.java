7c9e4c29be833ae2ed250f51965faf9cd959b814
sonhyejin <sjh8205@naver.com>
2018-11-05 19:37:31 +0900

    head ui 수정

---
+++ app/src/main/java/com/example/sonhyejin/save_shuttlebus/H_Wait.java
@@ -0,0 +1,13 @@
+package com.example.sonhyejin.save_shuttlebus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class H_Wait extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_h_wait);
+    }
+}
