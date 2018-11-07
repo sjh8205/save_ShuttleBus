9970b5471a4d5fbfad0adf9608b4232d38941b93
Bang Seunghee <bsh1162@gmail.com>
2018-11-08 00:56:05 +0900

    로그인 즁 디바이스 번호 받습니다,,
    테스트는 안해보무

--- app/src/main/java/com/example/sonhyejin/save_shuttlebus/H_Join.java
+++ app/src/main/java/com/example/sonhyejin/save_shuttlebus/H_Join.java
@@ -1,7 +1,12 @@
 package com.example.sonhyejin.save_shuttlebus;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
 
 public class H_Join extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class H_Join extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_h_join);
+
+        // 제출
+        // => 입력한 등록 번호, 이름 데이터ㅂㅔ이스에서 조회 후
+        // 있으면 있다고 안내 -> 뒤로 가기
+        // 없으면 넣기
+        // => 폰 번호도 받아야 함 (로그인 창에서 이미 받아옴) (원장의 경우만 회원가입)
     }
+
 }
--- app/src/main/java/com/example/sonhyejin/save_shuttlebus/Login.java
+++ app/src/main/java/com/example/sonhyejin/save_shuttlebus/Login.java
@@ -1,7 +1,12 @@
 package com.example.sonhyejin.save_shuttlebus;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +14,36 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        String tel_num = null;
+        TelephonyManager mgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        try {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            tel_num = mgr.getLine1Number();
+            tel_num = tel_num.replace("+82", "0");
+            Toast.makeText(getApplicationContext(), tel_num, Toast.LENGTH_SHORT).show();
+        } catch(Exception e) { } // 디바이스 번호 받아오기
+
+        // 에딧 텍스트 내 텍스트 => tel_num으로 바꾸고
+        // 텍스트 수정 못하게 하기
+
+        // 버튼 색깔 바꾸기
+
+        // 정보 넘겨서 데이터베이스 조회하기
+
+        // 데이터베이스에 존재하면 -> 메인 화면으로
+        // 아니면 -> 로그인 불가. -> 회원가입으로 가거나 하기
+
+
     }
 }
