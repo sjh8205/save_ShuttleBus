370d883299ee9966609ae9901677c08be1bb60d2
jeewoo9719 <34128041+jeewoo9719@users.noreply.github.com>
2018-11-08 22:12:25 +0900

    원장이 원생등록하는 부분
    gradle에 firebase Storage 와 zxing(qr 코드 생성을 위해)추가
    Manifest에 harwareAccelerated 추가 (qr코드 땜시)
    Register_child.java 디비에 값넣는거 객체로 관리하기 위해 생성
    H_Main_Register_child.java
    edittxt로 값읽고 버스 탈건지 버튼 값이랑 qr 코드 생성(generateQRCode()&toBitmap)후 값 db에 저장
    qr코드는 storage에 저장

---
+++ app/src/main/java/com/example/sonhyejin/save_shuttlebus/Register_child.java
@@ -0,0 +1,81 @@
+package com.example.sonhyejin.save_shuttlebus;
+
+public class Register_child {
+    public String childName;
+    public String childClass;
+    public String childPhoneNum;
+    public String childBusStation;
+    public Boolean childBus;
+    public Boolean childOnBus;
+    public String qrUri;
+
+    public Register_child(){}
+
+    public Register_child(String Name, String Class, String Phone, String Station,
+                          Boolean Ride, Boolean status,String qr){
+        this.childName=Name;
+        this.childClass=Class;
+        this.childPhoneNum=Phone;
+        this.childBusStation=Station;
+        this.childBus=Ride;
+        this.childOnBus=status;
+        this.qrUri=qr;
+    }
+
+    public void setChildName(String childName) {
+        this.childName = childName;
+    }
+
+    public void setChildClass(String childClass) {
+        this.childClass = childClass;
+    }
+
+    public void setChildPhoneNum(String childPhoneNum) {
+        this.childPhoneNum = childPhoneNum;
+    }
+
+    public void setChildBusStation(String childBusStation) {
+        this.childBusStation = childBusStation;
+    }
+
+    public void setChildBus(boolean childBus) {
+        this.childBus = childBus;
+    }
+
+    public void setChildOnBus(boolean childOnBus) {
+        this.childOnBus = childOnBus;
+    }
+
+    public void setQrUri(String qrUri) {
+        this.qrUri = qrUri;
+    }
+
+    public String getQrUri() {
+        return qrUri;
+    }
+
+    public String getChildName() {
+        return childName;
+    }
+
+    public String getChildClass() {
+        return childClass;
+    }
+
+    public String getChildPhoneNum() {
+        return childPhoneNum;
+    }
+
+    public String getChildBusStation() {
+        return childBusStation;
+    }
+
+    public Boolean isChildOnBus() {
+        return childOnBus;
+    }
+
+    public Boolean isChildBus() {
+        return childBus;
+    }
+
+}
