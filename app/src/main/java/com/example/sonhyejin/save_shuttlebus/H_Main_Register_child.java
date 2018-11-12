87bb7a1dd1ffe9ba158dd37028b3c51d28b2ea70
sonhyejin <sjh8205@naver.com>
2018-11-12 20:53:01 +0900

    reg_child walk/bus 버튼 연결

--- app/src/main/java/com/example/sonhyejin/save_shuttlebus/H_Main_Register_child.java
+++ app/src/main/java/com/example/sonhyejin/save_shuttlebus/H_Main_Register_child.java
@@ -4,6 +4,7 @@ import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
@@ -11,12 +12,40 @@ import android.widget.RadioGroup;
 public class H_Main_Register_child extends AppCompatActivity {
     boolean busRideStatus;
     EditText station;
+
+    Button busyes;
+    Button busno;
+    boolean status;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_h_main_register_child);
 
+        busyes = (Button)findViewById(R.id.busYes);
+        busno = (Button)findViewById(R.id.busNo);
 
+        busyes.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                status = true;
+                if(status==true){
+                    busyes.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.ClickedChooseButton));
+                    busno.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.ChooseButton));
+                }
+            }
+        });
+
+        busno.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                status = false;
+                if(status==false){
+                    busyes.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.ChooseButton));
+                    busno.setBackgroundColor(getApplicationContext().getResources().getColor(R.color.ClickedChooseButton));
+                }
+            }
+        });
 
         EditText childName = (EditText) findViewById(R.id.hRegChildName);
         EditText childClass = (EditText) findViewById(R.id.hRegChildClass);
