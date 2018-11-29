f6f85acd9b95d4c4f800072faf0f0959cd5b58ec
sonhyejin <sjh8205@naver.com>
2018-11-29 20:34:34 +0900

    t_main_qr 버튼 삭제

--- app/src/main/java/com/example/sonhyejin/save_shuttlebus/AdapterQR.java
+++ app/src/main/java/com/example/sonhyejin/save_shuttlebus/AdapterQR.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,67 +20,52 @@ import android.widget.TextView;
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 
-public class AdapterQR extends ArrayAdapter implements View.OnClickListener{
-    public interface ListBtnClickListener {
-        void onListBtnClick(int position) ;
+public class AdapterQR extends BaseAdapter{
+    private ArrayList<ListViewQR> listViewItemList = new ArrayList<ListViewQR>();
+
+    public AdapterQR(){ }
+
+    public int getCount(){
+        return listViewItemList.size();
     }
 
-    // 생성자로부터 전달된 resource id 값을 저장.
-    int resourceId ;
-    // 생성자로부터 전달된 ListBtnClickListener  저장.
-    private ListBtnClickListener listBtnClickListener ;
-
-
-    // ListViewBtnAdapter 생성자. 마지막에 ListBtnClickListener 추가.
-    AdapterQR(Context context, int resource, ArrayList<ListViewQR> list, ListBtnClickListener clickListener) {
-        super(context, resource, list) ;
-
-        // resource id 값 복사. (super로 전달된 resource를 참조할 방법이 없음.)
-        this.resourceId = resource ;
-
-        this.listBtnClickListener = clickListener ;
-    }
-
-    // 새롭게 만든 Layout을 위한 View를 생성하는 코드
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        final int pos = position ;
+    public View getView(int position, View convertView, ViewGroup parent){
+        final int pos = position;
         final Context context = parent.getContext();
 
-        // 생성자로부터 저장된 resourceId(listview_btn_item)에 해당하는 Layout을 inflate하여 convertView 참조 획득.
-        if (convertView == null) {
+        if(convertView == null){
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(this.resourceId/*R.layout.listview_btn_item*/, parent, false);
+            convertView = inflater.inflate(R.layout.layout_attendance, parent, false);
         }
 
-        // 화면에 표시될 View(Layout이 inflate된)로부터 위젯에 대한 참조 획득
-        final ImageView cState = (ImageView) convertView.findViewById(R.id.checkChildState);
-        final TextView cClass = (TextView) convertView.findViewById(R.id.checkChildClass);
-        final TextView cName = (TextView) convertView.findViewById(R.id.checkChildName);
+        ImageView imgView = (ImageView) convertView.findViewById(R.id.checkChildState);
+        TextView nameView = (TextView) convertView.findViewById((R.id.checkChildName));
+        TextView classView = (TextView) convertView.findViewById((R.id.checkChildClass));
 
-        // Data Set(listViewItemList)에서 position에 위치한 데이터 참조 획득
-        final ListViewQR listViewItem = (ListViewQR) getItem(position);
+        ListViewQR listViewItem = listViewItemList.get(position);
 
-        // 아이템 내 각 위젯에 데이터 반영
-        cState.setImageDrawable(listViewItem.getimg());
-        cName.setText(listViewItem.getsName());
-        cClass.setText(listViewItem.getsClass());
+        imgView.setImageDrawable((listViewItem.getimg()));
+        nameView.setText(listViewItem.getsName());
+        classView.setText(listViewItem.getsClass());
 
-        // button1 클릭 시 TextView(textView1)의 내용 변경.
-        Button btn = (Button) convertView.findViewById(R.id.checkChildNone);
-        btn.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                //cState.setImageDrawable(ContextCompat.getDrawable(H_Main_View_Teacher.class,R.drawable.busstop));
-            }
-        });
         return convertView;
     }
 
-    public void onClick(View v) {
-        // ListBtnClickListener(MainActivity)의 onListBtnClick() 함수 호출.
-        if (this.listBtnClickListener != null) {
-            this.listBtnClickListener.onListBtnClick((int)v.getTag()) ;
-        }
+    public long getItemId(int position){
+        return position;
     }
 
+    public Object getItem(int position){
+        return listViewItemList.get(position);
+    }
+
+    public void addItem(String name, String sclass, Drawable image){
+        ListViewQR item = new ListViewQR();
+
+        item.setimg(image);
+        item.setsName(name);
+        item.setsClass(sclass);
+
+        listViewItemList.add(item);
+    }
 }
--- app/src/main/java/com/example/sonhyejin/save_shuttlebus/T_main_QRScan.java
+++ app/src/main/java/com/example/sonhyejin/save_shuttlebus/T_main_QRScan.java
@@ -1,68 +1,49 @@
 package com.example.sonhyejin.save_shuttlebus;
 
+import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class T_main_QRScan extends AppCompatActivity{
-//선생님은 원생들 qr 스캔하는 페이지?
-
-    public boolean loadItems(ArrayList<ListViewQR> list) {
-        ListViewQR item ;
-
-        if (list == null) {
-            list = new ArrayList<ListViewQR>() ;
-        }
-
-        // 아이템 생성.
-        item = new ListViewQR() ;
-        item.setimg(ContextCompat.getDrawable(this,R.drawable.busstop));
-        item.setsName("a") ;
-        item.setsClass("b") ;
-        list.add(item) ;
-
-        item = new ListViewQR() ;
-        item.setimg(ContextCompat.getDrawable(this,R.drawable.busstop));
-        item.setsName("c") ;
-        item.setsClass("d") ;
-        list.add(item) ;
-
-        item = new ListViewQR() ;
-        item.setimg(ContextCompat.getDrawable(this,R.drawable.busstop));
-        item.setsName("e") ;
-        item.setsClass("f") ;
-        list.add(item) ;
-
-        return true ;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_t_main_qrscan);
 
-        ListView child = (ListView)findViewById(R.id.tChildList);
+        final ListView qr = (ListView)findViewById(R.id.tChildList);
 
-        AdapterQR adapter;
-        ArrayList<ListViewQR> items = new ArrayList<ListViewQR>() ;
+        AdapterQR Adapter = new AdapterQR();
 
-        loadItems(items) ;
+        Adapter.addItem("지우", "매화",ContextCompat.getDrawable(this,R.drawable.busstop));
+        Adapter.addItem("혜진", "튤립",ContextCompat.getDrawable(this,R.drawable.busstop));
+        Adapter.addItem("승희", "장미",ContextCompat.getDrawable(this,R.drawable.busstop));
 
-        adapter = new AdapterQR(this, R.layout.layout_attendance, items,null) ;
+        qr.setAdapter(Adapter);
 
-        child.setAdapter(adapter);
-
-        child.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        qr.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView parent, View v, int position, long id) {
+            public void onItemClick(AdapterView parent, View view, int position, long id) {
+                ListViewQR item = (ListViewQR) parent.getItemAtPosition(position);
+                String row = (String) qr.getItemAtPosition(position);
+
+                String sName = item.getsName();
+                String sClass = item.getsClass();
+                Drawable img = item.getimg();
+
+                Toast.makeText(getApplicationContext(), "You selected : " + row, Toast.LENGTH_SHORT).show(); // 클릭한 해당 위치 받아오기
+                //받은 위치의 버스 이미지만 보이게 하고 없애기
+//                route.getItemAtPosition(row).getimg
 
             }
-        }) ;
+        });
 
     }
 }
