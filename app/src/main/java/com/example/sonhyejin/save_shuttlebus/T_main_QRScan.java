a583ae896c350c19a35236f371f94e5b384ca5b5
sonhyejin <sjh8205@naver.com>
2018-11-29 21:52:27 +0900

    qr scanner 추가

--- app/src/main/java/com/example/sonhyejin/save_shuttlebus/T_main_QRScan.java
+++ app/src/main/java/com/example/sonhyejin/save_shuttlebus/T_main_QRScan.java
@@ -6,25 +6,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.content.Intent;
 
-import java.util.ArrayList;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
-public class T_main_QRScan extends AppCompatActivity{
+public class T_main_QRScan extends AppCompatActivity {
+
+    private IntentIntegrator qrScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_t_main_qrscan);
 
-        final ListView qr = (ListView)findViewById(R.id.tChildList);
+        final ListView qr = (ListView) findViewById(R.id.tChildList);
+        Button scanner = (Button) findViewById(R.id.tQrScan);
+        Button submit = (Button) findViewById(R.id.tCompletScan);
+
+        qrScan = new IntentIntegrator(this);
 
         AdapterQR Adapter = new AdapterQR();
 
-        Adapter.addItem("지우", "매화",ContextCompat.getDrawable(this,R.drawable.busstop));
-        Adapter.addItem("혜진", "튤립",ContextCompat.getDrawable(this,R.drawable.busstop));
-        Adapter.addItem("승희", "장미",ContextCompat.getDrawable(this,R.drawable.busstop));
+        Adapter.addItem("지우", "매화", ContextCompat.getDrawable(this, R.drawable.busstop));
+        Adapter.addItem("혜진", "튤립", ContextCompat.getDrawable(this, R.drawable.busstop));
+        Adapter.addItem("승희", "장미", ContextCompat.getDrawable(this, R.drawable.busstop));
 
         qr.setAdapter(Adapter);
 
@@ -40,10 +49,43 @@ public class T_main_QRScan extends AppCompatActivity{
 
                 Toast.makeText(getApplicationContext(), "You selected : " + row, Toast.LENGTH_SHORT).show(); // 클릭한 해당 위치 받아오기
                 //받은 위치의 버스 이미지만 보이게 하고 없애기
-//                route.getItemAtPosition(row).getimg
+                //route.getItemAtPosition(row).getimg
 
             }
         });
+
+        scanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                qrScan.setPrompt("Scanning...");
+                //qrScan.setOrientationLocked(false);
+                qrScan.initiateScan();
+            }
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),T_main.class);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String str;
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if(result != null && resultCode == RESULT_OK) {
+            // if user scanned and the result is valid, do your stuff here
+            str = result.getContents().toString();
+            Toast.makeText(this, "Scanned: " + str, Toast.LENGTH_LONG).show();
+        } else {
+            // if user pressed back or there's error, do your stuff here
+            super.onActivityResult(requestCode, resultCode, data);
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+        }
 
     }
 }
