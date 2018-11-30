c0cf2b9f63a010e3452229fe7809c9c9987f49eb
sonhyejin <sjh8205@naver.com>
2018-11-30 13:53:16 +0900

    h_view_bus

--- app/src/main/java/com/example/sonhyejin/save_shuttlebus/H_Main_View_bus.java
+++ app/src/main/java/com/example/sonhyejin/save_shuttlebus/H_Main_View_bus.java
@@ -1,23 +1,74 @@
 package com.example.sonhyejin.save_shuttlebus;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class H_Main_View_bus extends AppCompatActivity {
+
+    String telNum;
+    ListView route;
+    AdapterRoute Adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_h_main_view_bus);
 
-        ListView route = (ListView)findViewById(R.id.hBusList);
+        route = (ListView)findViewById(R.id.hBusList);
+        Adapter = new AdapterRoute();
 
-        AdapterRoute Adapter = new AdapterRoute();
+        SharedPreferences data = getSharedPreferences("mydata", Context.MODE_PRIVATE);
+        telNum = data.getString("telnum","0");
+
+        FirebaseDatabase FD = FirebaseDatabase.getInstance();
+        DatabaseReference DR = FD.getReference("Kindergarten");
+
+        DR.child("025556666").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot data: dataSnapshot.child("child").getChildren()){
+                    String nam = data.child("name").getValue(String.class);
+                    String num = data.child("Phone").getValue(String.class);
+
+                    Adapter.addItem(ContextCompat.getDrawable(H_Main_View_bus.this,R.drawable.busstop),num);
+                }
+                route.setAdapter(Adapter);
+
+                route.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView parent, View view, int position, long id) {
+                        ListViewChild item = (ListViewChild) parent.getItemAtPosition(position);
+
+                        String nameStr = item.getpName();
+                        String classStr = item.getpClass();
+                        String numStr = item.getpNum();
+                    }
+                });
+
+                Log.v("print", "affor");
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+
+            }
+        });
+
+        /*
 
         Adapter.addItem(ContextCompat.getDrawable(this,R.drawable.busstop),"안양1단지");
         Adapter.addItem(ContextCompat.getDrawable(this,R.drawable.busstop),"안양2단지");
@@ -35,5 +86,6 @@ public class H_Main_View_bus extends AppCompatActivity {
                 Drawable img = item.getimg();
             }
         });
+        */
     }
 }
