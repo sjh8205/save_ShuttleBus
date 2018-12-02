08a141ab0f8906c82c1deaff168ee8ead364c699
jeewoo9719 <34128041+jeewoo9719@users.noreply.github.com>
2018-12-02 18:10:11 +0900

    버스 인덱스 있으면 화면 안넘어가게

--- app/src/main/java/com/example/sonhyejin/save_shuttlebus/H_Main_Register_bus.java
+++ app/src/main/java/com/example/sonhyejin/save_shuttlebus/H_Main_Register_bus.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,6 +21,7 @@ public class H_Main_Register_bus extends AppCompatActivity {
     String Station;
     String Time;
     String telNum;
+    int indexCheck=0;
 
     DatabaseReference databaseReference;
     @Override
@@ -60,18 +63,41 @@ public class H_Main_Register_bus extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 databaseReference=FirebaseDatabase.getInstance().getReference()
-                        .child("Kindergarten").child(telNum).child("bus");
+                        .child("Kindergarten").child("027778888").child("bus");
 
                 //Routine=tRoutine.getText().toString();
                 Station=tStation.getText().toString();
                 Time=tTime.getText().toString();
 
-                registerBus regB=new registerBus(Station,Time,false);
-                String str=Time+" "+Station;
-                databaseReference.child(str).setValue(regB);
+                databaseReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for(DataSnapshot data:dataSnapshot.getChildren()){
+                            String temp=data.getKey();
+                            Log.v("regBus",temp);
+                            int idx=temp.indexOf(" ");
+                            String num = temp.substring(0,idx);
+                            if(Time.equals(num)) {
+                                indexCheck++;
+                            }
+                        }
+                        if(indexCheck==0){
+                            registerBus regB=new registerBus(Station,Time,false);
+                            String str=Time+" "+Station;
+                            databaseReference.child(str).setValue(regB);
+                            Intent intent1=new Intent(getApplicationContext(),H_Main_Register.class);
+                            startActivity(intent1);
+                        }else{
+                            Toast.makeText(getApplicationContext(),"Enter another index!",Toast.LENGTH_SHORT).show();
+                        }
+                    }
 
-                Intent intent1=new Intent(getApplicationContext(),H_Main_Register.class);
-                startActivity(intent1);
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
             }
         });
     }
