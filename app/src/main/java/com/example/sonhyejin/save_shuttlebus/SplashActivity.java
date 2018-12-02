7a81fababe5bc959882be3ccd6e7243ebb16d600
Bang Seunghee <bsh1162@gmail.com>
2018-12-03 00:45:31 +0900

    P_main 다이얼로그 수정, T_main_QRScan 컴플리트 버튼
    - P_main 다이얼로그 수정 중
    - P_main 들어가짐 -> 그런데 두 번째 자동로그인 접속 때는 안된당... 인텐트 때문이라고 생각 중
    - T_main_QRScan 컴플리트 버튼 활성화 안 되던 거 수정! : 함수 위치가 잘못 있었던 거였음!

--- app/src/main/java/com/example/sonhyejin/save_shuttlebus/SplashActivity.java
+++ app/src/main/java/com/example/sonhyejin/save_shuttlebus/SplashActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 
 public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState){
@@ -18,23 +19,29 @@ public class SplashActivity extends Activity {
         }
 
         SharedPreferences data = getSharedPreferences("mydata", Context.MODE_PRIVATE);
-
+        Log.v("ㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴ", data.toString());
 
         SharedPreferences check = getSharedPreferences("login", Context.MODE_PRIVATE);
+        Log.v("ㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴ", check.toString());
         int checked = check.getInt("FirstorNot",0);
+        Log.v("ㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴ", String.valueOf(checked));
 
         if(checked==1){ //사용자가 사전에 '원장' 자격으로 로그인 한 경우
             startActivity(new Intent(this,H_Main.class));
             finish();
+            Log.v("ㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴ", "h");
         }else if(checked==2){//사용자가 사전에 '부모' 자격으로 로그인 한 경우
             startActivity(new Intent(this,P_main.class));
             finish();
+            Log.v("ㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴ", "p");
         }else if(checked==3){//사용자가 사전에 '교사' 자격으로 로그인 한 경우
             startActivity(new Intent(this,T_main.class));
             finish();
+            Log.v("ㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴ", "t");
         }else {//그 외의 경우
             startActivity(new Intent(this,Login.class));
             finish();
+            Log.v("ㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴ", "f");
         }
 
         //startActivity(new Intent(this,Login.class));
